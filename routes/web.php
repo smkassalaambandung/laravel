3abc42bfaf9d57ea04ed79c15bb399c8a20a8be6
Agung Wahyudi <olipiskandar@gmail.com>
2021-10-04 14:25:58 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 100;
 });
 
-Auth::routes();
+Route::get('laravel/framework', function () {
+    return 'hallo laravel';
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Parameter Wajib
+Route::get('/nama-framework/{nama}/{tahun}', function ($nama, $tahun) {
+    return 'Nama Framework yang dimasukan adalah: ' . $nama .
+        '<br>Tahun <b>Rilis</b> : ' . $tahun;
+});
+
+
+// Optional Parameter
+Route::get('/nama-saya-adalah/{nama?}', function ($nama = 'Wajib Diisi') {
+    return 'Nama Saya Adalah : ' . $nama;
+});
+
+// url : hitung luas segitiga
+// optional param : alas, tinggi
+// output : hasil luas segitiga nya :
+// alas = null : 1, tinggi = null : 1
+Route::get('/luas-segitiga/{alas?}/{tinggi?}', function ($alas = 1, $tinggi = 1) {
+    return 'Luas Segitiga : ' . ($alas * $tinggi) * 0.5;
+});
