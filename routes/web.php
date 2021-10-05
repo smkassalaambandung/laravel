082034dfaae8f72879b025ee63f8aada3c442fec
Agung Wahyudi <olipiskandar@gmail.com>
2021-10-05 15:54:40 +0700

    menambahkan template

--- routes/web.php
+++ routes/web.php
@@ -65,3 +65,7 @@ Route::get('profile', function () {
 // Route::get('profile/{nama}', function ($nama) {
 //     return view('profile.detail', ['nama_user' => $nama]);
 // });
+
+Route::get('album', function () {
+    return view('album');
+});
